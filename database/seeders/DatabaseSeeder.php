00ccd0765671464f44ad40e19fc28e79ed99bbbb
Afrozens <jesuszen2.6@gmail.com>
2023-06-28 12:15:33 -0400

    feat: upload files

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create()->each(function($user){
+            \App\Models\Product::factory(3)->create([
+                "user_id" => $user->id
+            ]);
+        });
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
